5435b11426d0829adca661d9eefb687e8d898554
navaneeswarops <navaneeswar.ops@gmail.com>
2026-06-28 11:08:44 +0530

    Update provider.tf

--- EKS/provider.tf
+++ EKS/provider.tf
@@ -1,13 +1,20 @@
 terraform {
-  required_version = ">= 1.5.0"
+  required_version = ">= 1.5"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.40"
+      version = ">= 6.28.0"
     }
   }
+
+  backend "s3" {
+    bucket = "cicd-terraform-eks-503726126666-us-east-1-an"
+    key    = "eks/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
+
 provider "aws" {
   region = "us-east-1"
 }
