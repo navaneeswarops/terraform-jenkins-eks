7ad4c89d79892b1489859579c46682cb2f47eeab
navaneeswarops <navaneeswar.ops@gmail.com>
2026-06-28 10:43:28 +0530

    Update backend.tf

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "cicd-terraform-eks"
+    bucket = "cicd-terraform-eks-503726126666-us-east-1-an"
     key    = "eks/terraform.tfstate"
     region = "us-east-1"
   }
